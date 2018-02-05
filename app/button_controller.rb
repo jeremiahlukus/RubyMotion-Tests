b9350b485d0483679e3d388f73bc0141e2f7ac28
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-02-04 22:07:30 -0500

    First

---
+++ app/button_controller.rb
@@ -0,0 +1,19 @@
+class ButtonController < UIViewController
+  def viewDidLoad
+    super
+
+    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @button.setTitle("Test me title!", forState:UIControlStateNormal)
+    @button.accessibilityLabel = "Test me!"
+    @button.sizeToFit
+    @button.frame = CGRect.new([10, 70], @button.frame.size)
+    self.view.addSubview(@button)
+
+    @button.addTarget(self, action:'tapped', forControlEvents:UIControlEventTouchUpInside)
+  end
+
+  def tapped
+    p "I'm tapped!"
+    @was_tapped = true
+  end
+end 
